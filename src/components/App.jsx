0e206ff699ed747037e4129e664d42982ef6bcec
Centralice <108938330+Centralice@users.noreply.github.com>
2024-09-14 15:32:21 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import Description from "./Description/Description";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
 
 export default function App() {
-  return <Description />;
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback feedback={feedback} />
+    </>
+  );
 }
